dd5c11b8eed49653bf9cd9ac4adde346e255dd39
Max Friedman <mfriedman@lyft.com>
2018-01-03 12:45:34 -0800

    More scouting

--- scout.js
+++ scout.js
@@ -40,5 +40,66 @@ function getNewTarget(creep) {
 }
 
 function scouting(creep) {
+  if (creep.room.controller && creep.room.controller.my) {
+    // I already own this room
+    return;
+  }
 
+  if (!Memory.scoutInfo) {
+    Memory.scoutInfo = {};
+  }
+
+  if (creep.room.name in Memory.scoutInfo && Math.random() < 0.99) {
+    return;
+  }
+
+  var available = isRoomAvailable(creep);
+  if (!available) {
+    Memory.scoutInfo[creep.room.name] = { sources: 0 };
+    return;
+  }
+
+  Memory.scoutInfo[creep.room.name] = {
+    sources: countSources(creep)
+  };
+}
+
+function isRoomAvailable(creep) {
+  if (creep.room.controller) {
+    if (creep.room.controller.owner || creep.room.controller.reservation) {
+      return false;
+    }
+  }
+
+  var hostileTowers = countHostileTowers(creep);
+  if (hostileTowers > 0) {
+    return false;
+  }
+
+  var hostileAttackCreeps = countHostileAttackCreeps(creep);
+  if (hostileAttackCreeps > 0) {
+    return false;
+  }
+
+  return true;
+}
+
+function countHostileTowers(creep) {
+  return creep.room.find(FIND_HOSTILE_STRUCTURES, {
+    filter: function(object) {
+      return object.structureType == STRUCTURE_TOWER;
+    }
+  });
+}
+
+function countHostileAttackCreeps(creep) {
+  return creep.room.find(FIND_HOSTILE_CREEPS, {
+    filter: function(enemy) {
+      return enemy.getActiveBodyparts(ATTACK) > 0 || enemy.getActiveBodyparts(RANGED_ATTACK) > 0;
+    }
+  });
+}
+
+function countSources(creep) {
+  return creep.room.find(FIND_SOURCES).length;
 }
