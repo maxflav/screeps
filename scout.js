147a794287f807a4d69a665b0bdba43e79645254
Max Friedman <maxcfriedman@gmail.com>
2018-01-02 22:44:42 -0800

    more scouting

--- scout.js
+++ scout.js
@@ -6,16 +6,37 @@ module.exports = function(creep) {
 }
 
 function movement(creep) {
-  if (creep.memory.to) {
-    var moved = creep.moveTo(new RoomPosition(creep.memory.to.x, creep.memory.to.y, creep.memory.to.room));
-    if (moved == OK) {
-      return;
-    }
+  if (!tryToMove(creep)) {
+    creep.memory.to = getNewTarget(creep);
+  }
+  if (!tryToMove(creep)) {
+    utils.wander(creep, false);
+  }
+}
+
+function tryToMove(creep) {
+  if (!creep.memory.to) {
+    return false;
+  }
+  if (Math.random() < 0.01) {
+    // Don't get stuck forever
+    return false;
+  }
+  if (creep.room.name == creep.memory.to.room && Math.random() < 0.25) {
+    // I'm already in the room I wanted to go to
+    return false;
   }
 
-  utils.wander(creep, false);
+  var moved = creep.moveTo(new RoomPosition(creep.memory.to.x, creep.memory.to.y, creep.memory.to.room));
+  if (moved == OK) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-  // creep.room.
+function getNewTarget(creep) {
+  return null;
 }
 
 function scouting(creep) {
