5f98d30ea3251af8f739adde1bba19177a93ba00
Max Friedman <mfriedman@lyft.com>
2018-03-11 10:11:00 -0700

    Fix remote mines bug

--- creep.js
+++ creep.js
@@ -279,7 +279,7 @@ function getNewTargetRemoteMine(creep) {
   debug(creep, "Considering these remote mines " + JSON.stringify(roomNames));
 
   var picked = utils.minimize(roomNames, function(room) {
-    var assignedHere = globals.getTargetCount(name);
+    var assignedHere = globals.getTargetCount(room);
     var numSources = Memory.scoutInfo[name].sources;
     var dist = utils.roomDist(room, creep.room);
 
