1d4f856f5a861145d919930a3a63eaf198752ab7
Max Friedman <mfriedman@lyft.com>
2018-03-10 22:13:27 -0800

    roomDist

--- utils.js
+++ utils.js
@@ -1,12 +1,3 @@
-/*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('utils');
- * mod.thing == 'a thing'; // true
- */
-
 var utils = {};
 
 utils.distancePos = function(a, b) {
@@ -51,6 +42,30 @@ utils.maximize = function(list, func) {
   });
 
   return bestItem;
+}
+
+var roomRegex = /([EW])([0-9]+)([NS])([0-9]+)/;
+
+utils.roomDist = function(room1, room2) {
+  var parts1 = roomRegex.exec(room1);
+  var parts2 = roomRegex.exec(room2);
+
+  if (!parts1 || !parts2 || parts1.length < 5 || parts2.length < 5) {
+    console.log("Couldn't decode room names " + room1 + ", " + room2);
+    return 10000;
+  }
+
+  function coord(dir, num) {
+    var sign = 1;
+    if (dir == "W" || dir == "S") {
+      sign = -1;
+    }
+    return sign * num;
+  }
+
+  var xdist = Math.abs(coord(parts1[1], parts1[2]) - coord(parts2[1], parts2[2]));
+  var ydist = Math.abs(coord(parts1[3], parts1[4]) - coord(parts2[3], parts2[4]));
+  return xdist + ydist;
 }
 
 module.exports = utils;
